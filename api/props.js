4f32d3daa24f5fda01999a0eb4343ce4e1e4368b
agusssrs <agustinmancini2@hotmail.com>
2025-07-06 17:34:04 -0300

    dfasgfdssfzs# Please enter the commit message for your changes. Lines starting

--- api/props.js
+++ api/props.js
@@ -1,9 +1,15 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
+  // 🛡 Habilitar CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
+  if (req.method === "OPTIONS") {
+    res.status(200).end(); // Respuesta rápida al preflight
+    return;
+  }
 
   try {
     const response = await fetch('https://api.argencasas.com/props?api_key=6b8d4ba4dfdeadbe7ffe3ba8e40cb162');
