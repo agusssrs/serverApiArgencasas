b1ad08345920e91f3ffeb5c9d3dd0054b26c69ee
agusssrs <agustinmancini2@hotmail.com>
2025-07-06 17:28:38 -0300

    sdffsd

--- api/props.js
+++ api/props.js
@@ -1,6 +1,9 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // ← esto es clave
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+
   try {
     const response = await fetch('https://api.argencasas.com/props?api_key=6b8d4ba4dfdeadbe7ffe3ba8e40cb162');
     const data = await response.json();
