d18782e0ce78510bf523bd034a79f6f06706ba42
agusssrs <agustinmancini2@hotmail.com>
2025-07-06 17:21:20 -0300

    sasdfasas

---
+++ api/props.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch';
+
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('https://api.argencasas.com/props?api_key=6b8d4ba4dfdeadbe7ffe3ba8e40cb162');
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: 'Error al obtener datos de la API' });
+  }
+}
