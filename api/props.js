4be3b84743cd45177b429f095389952176494fed
agusssrs <agustinmancini2@hotmail.com>
2025-07-06 17:38:29 -0300

    dafadfsafsdfads

--- api/props.js
+++ api/props.js
@@ -1,13 +1,12 @@
 import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-  // 🛡 Habilitar CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
-    res.status(200).end(); // Respuesta rápida al preflight
+    res.status(200).end();
     return;
   }
 
